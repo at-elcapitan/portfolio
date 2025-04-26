bc56c24dc12748ff9107da53654e7f9107d4ff01
Vladislav 'ElCapitan' Nazarov <96237569+at-elcapitan@users.noreply.github.com>
2025-04-27 00:43:51 +0300

    about, quotes

--- src/js/app.js
+++ src/js/app.js
@@ -3,42 +3,100 @@ const { createI18n, useI18n } = VueI18n;
 
 const messages = {
     en: {
-        navb1: "About",
-        navb2: "Projects",
-        navb3: "Links",
-        hello1: "Hi, I'm",
-        hello_name: "Vladislav",
-        hello2: "Guitarist",
-        hello3: "Dreamer",
-        hello4: "Programmer",
-        infocard: "USA, Yosemite National Park, El Capitan Mountain",
-        quoter_header: "Favourite",
-        quoter_header2: "quotes",
-        quote1: "We are the sum of all our actions, salvation is within.",
-        quote2: "If you want to do something extraordinary, something much greater than predictable, you need extraordinary conditions.",
-        quote3: "Any excessively general statement in biology is false, including this one.",
-        author1: "Alek OS",
-        author2: "Andrey Doronichev",
-        author3: "Alexander Panchin"
+        nav : {
+            buttons : {
+                1: "About",
+                2: "Projects",
+                3: "Links"
+            }
+        },
+        hello: {
+           parts: {
+                1: "Hi, I'm",
+                2: "Guitarist",
+                3: "Dreamer",
+                4: "Programmer",
+                name: "Vladislav"
+           },
+            infocard: "USA, Yosemite National Park, El Capitan Mountain",
+        },
+        quoter: {
+            header1: "Favourite",
+            header2: "quotes",
+            quotes: {
+                1: {
+                    text: "We are the sum of all our actions, salvation is within.",
+                    author: "Alek OS, youtuber"
+                },
+                2: {
+                    text: "If you want to do something extraordinary, something much greater than predictable, you need extraordinary conditions.",
+                    author: "Andrey Doronichev, developer"
+                },
+                3: {
+                    text: "Any excessively general statement in biology is false, including this one.",
+                    author: "Alexander Panchin, scientist"
+                },
+                4: {
+                    text: "Even if you were born and raised in a small town, you always have a chance to make it to the very top.",
+                    author: "Yuri Dud, journalist"
+                },
+                5: {
+                    text: "Where is strength? In the ability to see objective reality, to fight for the truth, to separate it from the superficial. In the end, yes, strength is in the truth. And in believing that the truth exists and can be established.",
+                    author: "Leonid Volkov, director of ACF"
+                }
+            }
+        },
+        about: {
+            header: "About me"
+        }
     },
     ua: { 
-        navb1: "Про мене",
-        navb2: "Мої Проєкти",
-        navb3: "Контакти",
-        hello1: "Привіт, я",
-        hello_name: "Владислав",
-        hello2: "Гітарист",
-        hello3: "Мрійник",
-        hello4: "Програміст",
-        infocard: "США, Національний парк Йосеміті, Гора Ель Капітан",
-        quoter_header: "Улюблені",
-        quoter_header2: "цитати",
-        quote1: "Ми - всіх своїх дій сума, спасіння всередині.",
-        quote2: "Якщо ви хочете зробити щось незвичайне, що набагато більше, ніж передбачуване — потрібні незвичайні умови.",
-        quote3: "Будь-яке надмірно загальне твердження в біології завідомо хибне, включаючи це.",
-        author1: "Alek OS",
-        author2: "Андрій Дороничев",
-        author3: "Олександр Панчин"
+        nav: {
+            buttons: {
+                1: "Про мене",
+                2: "Мої проєкти",
+                3: "Посилання"
+            }
+        },
+        hello: {
+        parts: {
+                1: "Привіт, я",
+                2: "Гітарист",
+                3: "Мрійник",
+                4: "Програміст",
+              name: "Владислав"
+        },
+            infocard: "США, Національний парк Йосеміті, гора Ель-Капітан"
+        },
+        quoter: {
+            header1: "Улюблені",
+            header2: "цитати",
+            quotes: {
+            1: {
+                text: "Ми — сума всіх наших вчинків, порятунок всередині нас.",
+                author: "Alek OS, ютубер"
+              },
+            2: {
+                text: "Якщо хочете зробити щось надзвичайне, щось значно більше за передбачуване — потрібні надзвичайні умови.",
+                author: "Андрій Доронічев, розробник"
+              },
+            3: {
+                text: "Будь-яке надто загальне твердження в біології є хибним, включно з цим.",
+                author: "Олександр Панчин, вчений"
+              },
+            4: {
+                text: "Навіть якщо ви народилися й виросли в маленькому містечку, у вас завжди є шанс дістатися самого верху.",
+                author: "Юрій Дудь, журналіст"
+              },
+            5: {
+                text: "У чому сила? В умінні бачити об'єктивну реальність, боротися за правду, відділяти її від наносного. Зрештою, так, сила у правді. І у вірі, що правда існує та може бути встановлена.",
+                author: "Леонід Волков, директор ФБК"
+            }
+            }
+        },
+        about: {
+            header: "Про мене"
+        }
     },
     jp: {
         navb1: "自己紹介",
@@ -77,7 +135,7 @@ const app = createApp({
             }
 
             typedInstance.value = new Typed("#typing", {
-                strings: [t("hello2"), t("hello3"), t("hello4")],
+                strings: [t("hello.parts.2"), t("hello.parts.3"), t("hello.parts.4")],
                 typeSpeed: 100,
                 backSpeed: 50,
                 backDelay: 1000,
